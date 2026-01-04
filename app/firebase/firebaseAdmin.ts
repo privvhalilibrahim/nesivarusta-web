70126b5d4fac04d5d6cfcdd2f0a8bf74e90ed3d3
privvhalilibrahim <nesivarusta@gmail.com>
2026-01-04 18:22:37 +0100

    .

--- app/firebase/firebaseAdmin.ts
+++ app/firebase/firebaseAdmin.ts
@@ -1,10 +1,24 @@
 import admin from "firebase-admin"
-import serviceAccount from "./serviceAccountKey.json"
 
 if (!admin.apps.length) {
+  // Environment variable'dan service account bilgilerini al
+  const serviceAccountJson = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+  
+  if (!serviceAccountJson) {
+    throw new Error("FIREBASE_SERVICE_ACCOUNT_KEY environment variable is not set");
+  }
+
+  let serviceAccount: admin.ServiceAccount;
+  try {
+    // JSON string'i parse et
+    serviceAccount = JSON.parse(serviceAccountJson);
+  } catch (error) {
+    throw new Error("FIREBASE_SERVICE_ACCOUNT_KEY is not valid JSON");
+  }
+
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  })
+    credential: admin.credential.cert(serviceAccount),
+  });
 }
 
 export const db = admin.firestore()
